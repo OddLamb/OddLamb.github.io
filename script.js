8abb0102375c33897c1678502a012657f48fd82b
asaf1806 <asafmelo888@gmail.com>
2025-11-05 22:12:31 -0300

    Added Footer; Added Certificates Section

--- script.js
+++ script.js
@@ -5,7 +5,39 @@ const projects = [
         "img": "64-casas-thumbnail.png",
         "url": "https://github.com/OddLamb/64-casas"
     }
-]
+];
+const certificates = [
+    {
+        "title": "IA para Todos",
+        "enterprise": "StartSe University",
+        "date": "09/25",
+        "url": "https://lms.startse.com/certificado/v2/66d8620a181083001247fa9c?studentId=68cb31d10ec6f0ff59c762bf&trk=public_profile_see-credential"
+    },
+    {
+        "title": "Foundational C# with Microsoft",
+        "enterprise": "freeCodeCamp",
+        "date": "11/24",
+        "url": "https://www.freecodecamp.org/certification/OddLamb/foundational-c-sharp-with-microsoft?trk=public_profile_see-credential"
+    },
+    {
+        "title": "Responsive Web Design",
+        "enterprise": "freeCodeCamp",
+        "date": "11/24",
+        "url": "https://www.freecodecamp.org/certification/oddlamb/responsive-web-design?trk=public_profile_see-credential"
+    }
+];
+certificates.forEach((certificate) => {
+    const projectList = document.querySelector("#certificate-list");
+    projectList.innerHTML += `
+        <li>
+            <p><b>${certificate.title}</b></p>
+            <p><i>${certificate.enterprise}</i></p>
+            <p>Date of emission: ${certificate.date}</p>
+            <a href="${certificate.url}">
+                See Credencial
+            </a>
+        </li>`
+})
 projects.forEach((project) => {
     const projectList = document.querySelector("#project-list");
     projectList.innerHTML += `
